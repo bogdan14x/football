17ba08cfd199f4c4cff0c65b598d930d8edea374
Bogdan-Paul Bujor <bpbujor1@sheffield.ac.uk>
2016-04-11 17:57:21 +0100

    changed to modulus db

--- mongo.js
+++ mongo.js
@@ -1,5 +1,9 @@
+
+process.env['MONGO_USER'] = "bogdan14x";
+process.env['MONGO_PASS'] = "awesomesauce";
+
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/twitball');
+mongoose.connect('mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PASS+'@jello.modulusmongo.net:27017/igeje8Hi');
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
